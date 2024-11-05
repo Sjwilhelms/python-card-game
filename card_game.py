74b3b770b6d98e74f464e2b109a410660b8f9671
Sam Williams <sjwilliams388@gmail.com>
2024-11-05 14:24:54 +0000

    initial commit

---
+++ card_game.py
@@ -0,0 +1,31 @@
+import os
+import random
+from colorama import Fore, Back, Style
+
+# clear console
+
+os.system("clear")
+
+# define the suits and the numbers
+
+# define player related variables
+
+# create deck of cards
+
+###########
+
+# play the game
+
+# deal players
+
+# remove dealt cards from the deck
+
+# display player cards
+
+# compare the cards
+
+# add the dealt cards to the winners deck
+
+# display the number of cards left in the deck
+
+# determine the winner of the hand/game
